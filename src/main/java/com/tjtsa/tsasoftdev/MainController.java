0743868e83b2023accef290d17e0284c564dee7e
Michael Krause <mekva6274@gmail.com>
2016-04-22 14:41:22 -0400

    layout updates

--- src/main/java/com/tjtsa/tsasoftdev/MainController.java
+++ src/main/java/com/tjtsa/tsasoftdev/MainController.java
@@ -10,17 +10,35 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainController implements Initializable {
-    
+        
     @FXML
-    private Label label;
+    private TabPane tabpane;
+    @FXML
+    private Tab recentTab;
+    @FXML
+    private Tab uploadTab;
+    @FXML
+    private Accordion subjects;
+    @FXML
+    private Button addNewSubject;
+    @FXML
+    private TextField subjectName;
+ 
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {
-        System.out.println("loggingOut");
+        System.out.println("logging out...");
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
         Parent AssignmentParent = FXMLLoader.load(getClass().getResource("/fxml/AuthScene.fxml"));
         Scene AssignmentScene = new Scene(AssignmentParent);
@@ -29,8 +47,21 @@ public class MainController implements Initializable {
         stage.show(); 
     }
     
+    private void updateSubjects(TitledPane newPane){
+        subjects.getPanes().add(newPane);
+        System.out.println(subjects.getPanes());
+    }
+    
+    @FXML
+    private void addSubject(ActionEvent event){
+        updateSubjects(new TitledPane(subjectName.getText(), new AnchorPane()));
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        //LOAD ALL RELEVANT INFO UPON ENTERING.
+        
     }    
 }
